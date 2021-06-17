52e2ac60ce0f62c45bbf2544951ab2e0054ad28b
imadcollin <imad.collin@gmail.com>
2021-06-17 18:16:36 +0200

    Add other intro-example

--- Panda-series.py
+++ Panda-series.py
@@ -25,4 +25,24 @@ print(csv_header.shape)
 
 # Data attributes
 print(csv_header.dtypes)
+
+# %%
+# Statiscal Summary of Data
+from pandas import read_csv
+from pandas import set_option
+
+data = read_csv('data.csv')
+salaries = data['salary']
+
+set_option('display.width', 100)
+set_option('precision', 2)
+print(salaries.shape)
+print(salaries.describe())
+
+# Skew
+print(salaries.skew())
+
+# Class Distribution
+countDept = data.groupby('dept').size()
+print('\n', countDept)
 # %%
