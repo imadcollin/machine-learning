85485ae7923045dfdade7db90941fdd757b3976e
imadcollin <imad.collin@gmail.com>
2021-06-21 23:51:43 +0200

    Add normalization example

--- Preparing-data.py
+++ Preparing-data.py
@@ -8,8 +8,26 @@ names = [
     'preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class'
 ]
 data = read_csv('diabetes-india.csv', names=names)
-print(data.values)
+print("\Values:\n", data.values)
 
 data_scaler = preprocessing.MinMaxScaler(feature_range=(0, 1))
 data_rescaled = data_scaler.fit_transform(data)
-print(data_rescaled)
+print("\nScaled data:\n", data_rescaled)
+
+## Normalization
+from sklearn.preprocessing import Normalizer
+
+data_norm = Normalizer(norm='l1', copy=True).fit(data)
+Data_normed = data_norm.transform(data)
+print("\nNormalized data L1:\n", Data_normed)
+### Normalized with precision
+set_printoptions(precision=2)
+print("\nNormalized data precision:\n", Data_normed)
+
+#### Normalization L2
+data_norm = Normalizer(norm='l2', copy=False).fit(data)
+Data_normed = data_norm.transform(data)
+print("\nNormalized data L2:\n", Data_normed)
+### Normalized with precision
+set_printoptions(precision=2)
+print("\nNormalized data precision:\n", Data_normed)
