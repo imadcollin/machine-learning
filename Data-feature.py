97c42d7039bc5c03d59e9b5d1e2e4cb8c0f3cdb4
imadcollin <imad.collin@gmail.com>
2021-06-24 23:39:33 +0200

    Add feature importance

--- Data-feature.py
+++ Data-feature.py
@@ -2,6 +2,7 @@
 # Univariate Selection
 import numpy as np
 from pandas import read_csv
+from pandas.core.algorithms import mode
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from numpy import set_printoptions
@@ -21,5 +22,22 @@ print("\nScores\n", fit.scores_)
 
 featured_data = fit.transform(x)
 print("\nFeatured Data\n", featured_data[0:10])
+
+# %%
+from pandas import read_csv
+from sklearn.ensemble import ExtraTreesClassifier
+
+names = [
+    'preg', 'plas', 'pres', 'skin', 'test', 'mass', 'pedi', 'age', 'class'
+]
+data = read_csv('diabetes-india.csv', names=names)
+
+print("\Values:\n", data[:10])
+array = data.values
+x = array[:, 0:8]
+y = array[:, 8]
+model = ExtraTreesClassifier()
+model.fit(x, y)
+print("\nModel\n", model.feature_importances_)
 
 # %%
