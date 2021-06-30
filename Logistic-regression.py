cb2e9af38d6858448c6e2dfc1376faebeee30ea5
imadcollin <imad.collin@gmail.com>
2021-06-30 22:21:01 +0200

    Multinomial example

--- Logistic-regression.py
+++ Logistic-regression.py
@@ -1,6 +1,7 @@
 #%%
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.sparse import data
 import seaborn as sns
 from sklearn import datasets
 
@@ -13,3 +14,22 @@ plt.scatter(x[y == 1][:, 0], x[y == 1][:, 1], color='y', label='1')
 
 plt.legend()
 # %%
+# Multinomial Logistic Regression Model
+from sklearn import datasets
+from sklearn import linear_model
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+
+digits = datasets.load_digits()
+x = digits.data
+y = digits.target
+x_train, x_test, y_train, y_test = train_test_split(x,
+                                                    y,
+                                                    test_size=0.4,
+                                                    random_state=1)
+
+digreg = linear_model.LogisticRegression()
+digreg.fit(x_train, y_train)
+y_pred = digreg.predict(x_test)
+print("\n Logistic regrassion model:  ",
+      metrics.accuracy_score(y_test, y_pred) * 100)
